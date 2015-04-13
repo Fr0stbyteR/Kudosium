bdcf4fcca33b7594476586a1ab566e407dc58b4d
Fr0stbyteR <renshihong@hotmail.com>
2015-04-13 03:19:00 +0200

    update

--- admin/setup_db.php
+++ admin/setup_db.php
@@ -1,7 +1,9 @@
 <?php
 include_once "../config.inc.php";
+/*事件表*/
 $sql = "CREATE TABLE event_main
 (
+/*ID和主体*/
 id int,
 title text,
 main_verb text,
@@ -9,8 +11,14 @@ main_noun text,
 place text,
 with_user text,
 note text,
+/*bool属性*/
+is_social tinyint(1),
 is_whole_day tinyint(1) DEFAULT 0,
 is_instantaneous tinyint(1) DEFAULT 0,
+/*社交事件属性*/
+publisher text,
+permission text,
+/*日历属性*/
 start_time datetime,
 end_time datetime,
 duration bigint,
@@ -20,6 +28,7 @@ repetition bigint,
 repetition_end_time datetime,
 notification_time_before int,
 other_amount text,
+/*后期属性*/
 rate longtext,
 skill_node text,
 PRIMARY KEY (id)
@@ -27,7 +36,7 @@ PRIMARY KEY (id)
 $result = mysql_query($sql);
 echo $result;
 
-
+/*成就表*/
 $sql = "CREATE TABLE ach_main
 (
 id int NOT NULL AUTO_INCREMENT,
@@ -35,18 +44,20 @@ is_social tinyint(1) DEFAULT 0,
 title text,
 main_verb text,
 main_noun text,
+amount text,
 PRIMARY KEY (id)
 )";
 $result = mysql_query($sql);
 echo $result;
 
-
+/*用户表*/
 $sql = "CREATE TABLE user_main
 (
 id int NOT NULL AUTO_INCREMENT,
 email varchar(64),
 password char(64),
 name varchar(15),
+/*个人信息*/
 gender tinyint(2) UNSIGNED DEFAULT 0,
 birthday datetime,
 lastname varchar(32),
@@ -60,15 +71,17 @@ blog varchar(128),
 tag text,
 has_avatar tinyint(1),
 avatar varchar(128),
-sns_notification longtext,
-login_count int UNSIGNED,
 other text,
+/*SNS*/
+sns_notification longtext,
 following longtext,
 follower longtext,
 private_ach_points int UNSIGNED,
 social_ach_points int UNSIGNED,
 skill_points int UNSIGNED,
 todo_list text,
+/*内部*/
+login_count int UNSIGNED,
 reg_time datetime,
 last_login_time datetime,
 PRIMARY KEY (id)
@@ -76,7 +89,7 @@ PRIMARY KEY (id)
 $result = mysql_query($sql);
 echo $result;
 
-
+/*登录记录*/
 $sql = "CREATE TABLE access_log
 (
 id int NOT NULL AUTO_INCREMENT,
@@ -84,6 +97,16 @@ user_id int,
 user_ip varchar(64),
 PRIMARY KEY (id)
 )";
+$result = mysql_query($sql);
+echo $result;
+
+/*技能表*/
+$sql = "CREATE TABLE skill_node
+(
+id int NOT NULL AUTO_INCREMENT,
+PRIMARY KEY (id)
+)";
+
 $result = mysql_query($sql);
 echo $result;
 
